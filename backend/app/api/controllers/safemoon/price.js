0f470e52e18bfd887be0c9ef2c0deaa406d21f79
SuperDev348 <vladimir.danil25@mail.ru>
2022-07-07 04:52:38 -0800

    some fix in price controller

--- backend/app/api/controllers/safemoon/price.js
+++ backend/app/api/controllers/safemoon/price.js
@@ -2,27 +2,6 @@ const axios = require('axios');
 const priceModel = require('../../models/safemoon/price');	
 
 module.exports = {
-  getPrice: function(req, res, next) {
-    const startTime = new Date();
-    startTime.setHours(0);
-    startTime.setMinutes(0);
-    const current = new Date();
-    priceModel.find({ 
-      $and: [ { Timestamp: { $gte : startTime } }, { Timestamp: { $lte : current } }] 
-    })
-    .sort({Timestamp: -1})
-    .exec()
-    .then((result) => {
-      if (result.length === 0)
-        res.status(400).json({ msg: "Not found" });
-      else {
-        res.status(200).json({msg: "Found!", data: result[0]});
-      }
-    })
-    .catch(error =>  {
-      res.status(400).json({ msg: "Not found" });
-    })
-  },
   getPrices: function(req, res, next) {
     const startTime = new Date();
     startTime.setDate(startTime.getDate() - 7 + 1)
