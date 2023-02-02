2665cd2f7b4d9d971f4f509deba112650a82da41
SuperDev348 <vladimir.danil25@mail.ru>
2023-02-02 15:29:09 +0600

    update render date picker

--- frontend/src/components/renderDatePicker.js
+++ frontend/src/components/renderDatePicker.js
@@ -4,7 +4,7 @@ import DatePicker from 'react-datepicker';
 const renderDatePicker = ({input, placeholder, defaultValue, meta: {touched, error} }) => (
   <div>
     <DatePicker {...input} dateForm="MM/DD/YYYY" selected={input.value ? input.value : null} />
-    {touched && error ? <div style={{color: "red", fontSize: 12}}>{error}</div> : <div style={{marginBottom: 18}}></div>}
+    
   </div>
 );
 
