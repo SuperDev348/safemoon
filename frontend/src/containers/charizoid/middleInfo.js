1f7b6cd8d1b8f98e8491e1417c294e802cae4ec0
SuperDev348 <vladimir.danil25@mail.ru>
2023-02-04 22:15:52 +0600

    update

--- frontend/src/containers/charizoid/middleInfo.js
+++ frontend/src/containers/charizoid/middleInfo.js
@@ -11,6 +11,10 @@ import {getByWalletId} from '../../api/charizoid/coin'
 import {useStyles} from '../style/material_ui_style'
 import WalletInfo from './walletInfo'
 import MarketInfo from './marketInfo'
+import EditEarning from './editEarning'
+import EarningsPerDay from './earningsPerDay'
+import Holder from './holder'
+import Comment from './comment'
 
 const earningTimeTexts = [
   '15 min',
