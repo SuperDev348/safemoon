359b7b579ae51a2da69c395ea454d0faa6980a5b
SuperDev348 <vladimir.danil25@mail.ru>
2023-04-06 07:31:27 +0200

    fix router

--- backend/routes/ass/coin.js
+++ backend/routes/ass/coin.js
@@ -5,7 +5,6 @@ const Controller = require('../../app/api/controllers/ass/coin');
 router.get('/', Controller.getAll);
 router.get('/:Id', Controller.getByWalletId);
 router.get('/day/:Id', Controller.getPerDay);
-router.put('/:Id', Controller.updateById);
 router.delete('/:Id', Controller.deleteByWalletId);
 router.post('/buy/', Controller.buy);
 router.post('/sell/', Controller.sell);
