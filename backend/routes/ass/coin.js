eb135ebe749d07501a0b1201956a5c86e260698c
SuperDev348 <vladimir.danil25@mail.ru>
2023-04-12 15:30:39 +0200

    updat price outer

--- backend/routes/ass/coin.js
+++ backend/routes/ass/coin.js
@@ -5,5 +5,9 @@ const Controller = require('../../app/api/controllers/ass/coin');
 router.get('/', Controller.getAll);
 router.get('/:Id', Controller.getByWalletId);
 router.get('/day/:Id', Controller.getPerDay);
+router.put('/:Id', Controller.updateById);
+router.delete('/:Id', Controller.deleteByWalletId);
+router.post('/buy/', Controller.buy);
+router.post('/sell/', Controller.sell);
 
 module.exports = router;
--- backend/routes/ass/price.js
+++ backend/routes/ass/price.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const Controller = require('../../app/api/controllers/ass/price');
 
 router.get('/', Controller.getPrice);
-router.get('/history', Controller.getPrices);
 
 module.exports = router;
