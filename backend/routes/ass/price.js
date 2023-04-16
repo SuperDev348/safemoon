7cb3fc498903046416b0578f7df51d402eed81ec
SuperDev348 <vladimir.danil25@mail.ru>
2023-04-16 08:08:20 +0200

    update price

--- backend/routes/ass/price.js
+++ backend/routes/ass/price.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../../app/api/controllers/ass/price');
 
-router.get('/', Controller.getPrice);
 router.get('/history', Controller.getPrices);
 
 module.exports = router;
