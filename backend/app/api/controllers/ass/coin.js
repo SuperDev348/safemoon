a254285bc293c4cf8fde9374dddbcd0966b06f8c
SuperDev348 <vladimir.danil25@mail.ru>
2023-05-05 15:38:56 +0200

    update coin

--- backend/app/api/controllers/ass/coin.js
+++ backend/app/api/controllers/ass/coin.js
@@ -28,6 +28,9 @@ const getDataByWalletId = async (walletId) => {
     {start: 7*24*60, end: 24*60},
     {start: 52*7*24*60, end: 24*7*60},
   ]
+  const currentDate = new Date()
+  // current amount
+  let currentAmount = NO_DATA
   const res = await axios.get('https://api.bscscan.com/api?'+ new URLSearchParams({
     module: 'account',
     action: 'tokenbalance',
@@ -42,16 +45,6 @@ const getDataByWalletId = async (walletId) => {
   currentAmount = Math.floor(res.data.result/1000000000);
   console.log(currentAmount)
   // amounts
-  let amounts = await  Promise.all(timeStamps.map( async (item) => {
-    let startTime = new Date(currentDate.getTime() - item.start*60000)
-    let endTime = new Date(currentDate.getTime() - item.end*60000)
-    const result = await getDataWithTime(walletId, startTime, endTime)
-    if (result.length == 0)
-      return NO_DATA
-    else {
-      return result[0].amount
-    }
-  }))
   amounts = [currentAmount, ...amounts]
   return amounts
 }
