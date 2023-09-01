54709663e7865411ace0c5285d6a7a2127dd8eec
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-01 13:52:34 +0200

    update auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,3 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('../app/api/controllers/users');
-router.post('/register', UserController.create);
-router.post('/login', UserController.login);
 module.exports = router;
