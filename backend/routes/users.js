7a0ac19293282dcd8b6d9df638c3a386a32905b3
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-02 13:52:18 +0200

    fix

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,4 +4,7 @@ const UserController = require('../app/api/controllers/users');
 router.post('/register', UserController.create);
 router.get('/userlist', UserController.userlist);
 router.get('/getall', UserController.getAll);
+router.get('/:userId', UserController.getById);
+router.put('/:userId', UserController.updateById);
+router.delete('/:userId', UserController.deleteById);
 module.exports = router;
