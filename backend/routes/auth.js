b69fcec46588af83d2d871ed631b83fb90a6f34c
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-03 13:51:44 +0200

    fix

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../app/api/controllers/users');
 router.post('/register', UserController.create);
+router.post('/login', UserController.login);
 module.exports = router;
