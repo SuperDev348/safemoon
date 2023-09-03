4637b5c2c05da3b921146c6f7aea0e91a3793332
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-03 13:51:14 +0200

    fix

--- backend/routes/safemoon/wallet.js
+++ backend/routes/safemoon/wallet.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../../app/api/controllers/safemoon/wallet');
 
+router.post('/', Controller.create);
+
 module.exports = router;
