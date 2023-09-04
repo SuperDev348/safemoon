9fddb61c5ad6bb3a2bc2f2d7654ca097636e5fe8
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-04 13:51:04 +0200

    update safemoon wallet

--- backend/routes/safemoon/wallet.js
+++ backend/routes/safemoon/wallet.js
@@ -2,6 +2,4 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../../app/api/controllers/safemoon/wallet');
 
-router.post('/', Controller.create);
-
 module.exports = router;
