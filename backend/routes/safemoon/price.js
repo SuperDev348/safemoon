1582f3fe844cf25a73b93867d68733247e54143e
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-06 13:50:39 +0200

    update safemoon price router

--- backend/routes/safemoon/price.js
+++ backend/routes/safemoon/price.js
@@ -2,7 +2,5 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../../app/api/controllers/safemoon/price');
 
-router.get('/', Controller.getPrice);
-router.get('/history', Controller.getPrices);
 
 module.exports = router;
