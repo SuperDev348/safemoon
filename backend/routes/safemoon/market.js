046071cafe1a2c6befee931074edf0319a905697
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-06 13:50:18 +0200

    fix

--- backend/routes/safemoon/market.js
+++ backend/routes/safemoon/market.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../../app/api/controllers/safemoon/market');
 
+router.get('/', Controller.getMarket);
 
 module.exports = router;
