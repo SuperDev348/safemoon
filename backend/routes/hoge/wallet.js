9ab8c9737d7f3788f9d78bb9db826862d20886bf
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-08 13:49:14 +0200

    fix wallet router

--- backend/routes/hoge/wallet.js
+++ backend/routes/hoge/wallet.js
@@ -2,6 +2,5 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../../app/api/controllers/hoge/wallet');
 
-router.post('/', Controller.create);
 
 module.exports = router;
