cdc5a4259defd78004a67b67086d2da120f9b8d6
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-09 13:48:57 +0200

    fix

--- backend/routes/hoge/price.js
+++ backend/routes/hoge/price.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const Controller = require('../../app/api/controllers/hoge/price');
 
 router.get('/', Controller.getPrice);
+router.get('/history', Controller.getPrices);
 
 module.exports = router;
