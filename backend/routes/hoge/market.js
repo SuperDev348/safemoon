95eeb8ac6860131c65dd46f21435970bd2cc5b67
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-10 13:48:45 +0200

    update price hoge router

--- backend/routes/hoge/market.js
+++ backend/routes/hoge/market.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../../app/api/controllers/hoge/market');
 
+router.get('/', Controller.getMarket);
+
 module.exports = router;
--- backend/routes/hoge/price.js
+++ backend/routes/hoge/price.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const Controller = require('../../app/api/controllers/hoge/price');
 
 router.get('/', Controller.getPrice);
-router.get('/history', Controller.getPrices);
 
 module.exports = router;
