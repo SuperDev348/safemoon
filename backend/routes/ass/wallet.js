d3d1928cbb080324f9ebb80fd783fa8c5672c0d8
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-10 13:48:09 +0200

    update coin hoge router

--- backend/routes/ass/wallet.js
+++ backend/routes/ass/wallet.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Controller = require('../../app/api/controllers/ass/wallet');
+
+
+module.exports = router;
--- backend/routes/hoge/coin.js
+++ backend/routes/hoge/coin.js
@@ -6,8 +6,5 @@ router.get('/', Controller.getAll);
 router.get('/:Id', Controller.getByWalletId);
 router.get('/day/:Id', Controller.getPerDay);
 router.put('/:Id', Controller.updateById);
-router.delete('/:Id', Controller.deleteByWalletId);
-router.post('/buy/', Controller.buy);
-router.post('/sell/', Controller.sell);
 
 module.exports = router;
