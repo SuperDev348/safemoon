768ac1a48a5b4bde6dc319da218ed49141a314aa
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-10 13:48:29 +0200

    fix market hoge

--- backend/routes/hoge/market.js
+++ backend/routes/hoge/market.js
@@ -2,6 +2,4 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../../app/api/controllers/hoge/market');
 
-router.get('/', Controller.getMarket);
-
 module.exports = router;
