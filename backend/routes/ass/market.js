96e870578fad9758e5149b54cc3ed843dbba0fc2
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-11 13:47:04 +0200

    fix

--- backend/routes/ass/market.js
+++ backend/routes/ass/market.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../../app/api/controllers/ass/market');
 
+router.get('/', Controller.getMarket);
 
 module.exports = router;
