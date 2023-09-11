270da30ba86e1792ebc8c530e10fe49da12e86ab
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-11 13:46:56 +0200

    update ass market router

--- backend/routes/ass/coin.js
+++ backend/routes/ass/coin.js
@@ -7,5 +7,7 @@ router.get('/:Id', Controller.getByWalletId);
 router.get('/day/:Id', Controller.getPerDay);
 router.put('/:Id', Controller.updateById);
 router.delete('/:Id', Controller.deleteByWalletId);
+router.post('/buy/', Controller.buy);
+router.post('/sell/', Controller.sell);
 
 module.exports = router;
--- backend/routes/ass/market.js
+++ backend/routes/ass/market.js
@@ -2,6 +2,5 @@ const express = require('express');
 const router = express.Router();
 const Controller = require('../../app/api/controllers/ass/market');
 
-router.get('/', Controller.getMarket);
 
 module.exports = router;
