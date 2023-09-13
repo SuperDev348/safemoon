eaba3a6ee8faf5d3cf1103c40901d84f35144b69
SuperDev348 <vladimir.danil25@mail.ru>
2023-09-13 13:45:13 +0200

    update safemoon price

--- backend/app/api/controllers/safemoon/price.js
+++ backend/app/api/controllers/safemoon/price.js
@@ -12,27 +12,11 @@ module.exports = {
     })
     .sort({Timestamp: -1})
     .exec()
-    .catch(error =>  {
-      res.status(400).json({ msg: "Not found" });
-    })
-  },
-  getPrices: function(req, res, next) {
-    const startTime = new Date();
-    startTime.setDate(startTime.getDate() - 7 + 1)
-    startTime.setHours(0);
-    startTime.setMinutes(0);
-    const current = new Date();
-    priceModel.find({ 
-      $and: [ { Timestamp: { $gte : startTime } }, { Timestamp: { $lte : current } }] 
-    })
-    .sort({Timestamp: 1})
-    .exec()
     .then((result) => {
       if (result.length === 0)
         res.status(400).json({ msg: "Not found" });
       else {
-        result = result.filter((item, index) => index%120 === 0)
-        res.status(200).json({msg: "Found!", data: result});
+        res.status(200).json({msg: "Found!", data: result[0]});
       }
     })
     .catch(error =>  {
